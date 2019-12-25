ee23c628f52c6866321781b6ccd1cdefc22b30dd
Renzo Pacheco <renzopachecoj@gmail.com>
2019-12-25 14:45:22 -0500

    final version

---
+++ sol_luna_clock/README.md
@@ -0,0 +1,9 @@
+# Digital Clock
+
+Sol/Luna Clock
+
+<img src='sol_luna.gif' width='350'>
+
+<img src='sol_luna_light.png' width='350'>
+
+<img src='sol_luna_dark.png' width='350'>
---
+++ sol_luna_clock/lib/main.dart
@@ -0,0 +1,40 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_clock_helper/customizer.dart';
+import 'package:flutter_clock_helper/model.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'sol_luna_clock.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); 
+  // A temporary measure until Platform supports web and TargetPlatform supports
+  // macOS.
+  if (!kIsWeb && Platform.isMacOS) {
+    // https://github.com/flutter/flutter/issues/31366
+    // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override.
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+
+  // This creates a clock that enables you to customize it.
+  //
+  // The [ClockCustomizer] takes in a [ClockBuilder] that consists of:
+  //  - A clock widget (in this case, [DigitalClock])
+  //  - A model (provided to you by [ClockModel])
+  // For more information, see the flutter_clock_helper package.
+  //
+  // Your job is to edit [DigitalClock], or replace it with your
+  // own clock widget. (Look in sol_luna_clock.dart for more details!)
+
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeRight, DeviceOrientation.landscapeLeft])
+      .then((_) {
+    runApp(ClockCustomizer((ClockModel model) => DigitalClock(model)));
+  });
+}
